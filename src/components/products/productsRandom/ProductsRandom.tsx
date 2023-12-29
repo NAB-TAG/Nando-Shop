c01eea5dcd3e5f0b49ab512c2c467f1875edb9ff
bruno10-0 <brunobrodon75@gmail.com>
2023-12-29 20:16:04 -0300

    feat: :lipstick: modificacion de aspecto a el componente de productos

--- src/components/products/productsRandom/ProductsRandom.tsx
+++ src/components/products/productsRandom/ProductsRandom.tsx
@@ -7,7 +7,7 @@ function ProductsRandom() {
         <>
             <div className="w-full h-auto my-2 p-2">
                 <div className="w-72 bg-base-200  drop-shadow-md rounded-lg">
-                    <img className="object-cover rounded-tl-lg rounded-tr-lg select-none"
+                    <img className="relative object-cover rounded-tl-lg rounded-tr-lg select-none"
                         src="https://www.lemeilleuravis.com/wp-content/uploads/2020/08/vibox-pyro-sa4-133-pc-gamer-10.jpg" />
                     <div className="px-5 py-3 space-y-2">
                         <h3 className="text-lg">Super computadora 64GB</h3>
@@ -16,7 +16,7 @@ function ProductsRandom() {
                         <p className="space-x-2">
                             <span className="text-2xl font-semibold text-primary">$600</span>
                             <span className="text-sm line-through text-gray-500">$1000</span>
-                            <span className="text-sm bg-primary p-1 rounded-lg">40% Descuento</span>
+                            <span className="text-sm bg-primary p-1 rounded-2xl top-0 right-1 absolute mt-2 badge-primary mx-10">40% off</span>
                         </p>
                         <div className="flex justify-between items-center pt-3 pb-2">
                             <a href="#"
