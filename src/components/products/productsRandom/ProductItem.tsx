b574ce6b6ebdefa79b7eab6f6433c19260b9b2ca
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-01-04 16:39:48 -0300

    refactor(products): :recycle: Refactorizando el codigo del productsRandom

---
+++ src/components/products/productsRandom/ProductItem.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import ProductRandom from "./ProductsRandom.module.css"
+import { FaHeart } from "react-icons/fa6";
+import { FaCartArrowDown } from "react-icons/fa6";
+
+const ProductItem = () => {
+    return(
+        <div className="min-[340px]:w-full min-[450px]:w-1/2 md:w-1/3 lg:w-1/4  bg-base-200 border-[10px] border-base-100 rounded-lg">
+            <div className={`${ProductRandom.conteinerImg} relative overflow-hidden rounded-tl-lg rounded-tr-lg`}>
+                <div className="h-56">
+                    <img
+                        className={`${ProductRandom.imgBx} hover:scale-110 transition-transform relative w-full h-full rounded-tl-lg rounded-tr-lg select-none object-contain`}
+                        src="/assets/images/products/3.png"
+                        alt="Descripción de la imagen"
+                    />
+                </div>
+                <ul className={`${ProductRandom.action} absolute`}>
+                    <li className="relative list-none w-10 h-10 bg-white flex justify-center items-center m-2 cursor-pointer text-sm rounded-lg group">
+                        <FaHeart className="text-primary" />
+                        <span className="absolute right-12 top-5 transform -translate-y-1/2 whitespace-nowrap px-2 py-2 bg-white text-primary rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ">Agregar a favoritos.</span>
+                    </li>
+                    <li className="relative list-none w-10 h-10 bg-white flex justify-center items-center m-2 cursor-pointer text-sm rounded-lg group">
+                        <FaCartArrowDown className="text-primary" />
+                        <span className="absolute right-12 top-5 transform -translate-y-1/2 whitespace-nowrap px-2 py-2 bg-white text-primary rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">Agregar al carrito.</span>
+                    </li>
+                    
+                </ul>
+
+            </div>
+
+
+            <div className="px-5 py-3 space-y-2">
+                <h3 className="text-lg">Super computadora 64GB</h3>
+                <div className="flex flex-row justify-between">
+                    <div className="space-x-2 flex flex-col">
+                        <span className="text-2xl font-semibold text-primary">$600</span>
+                        <span className="text-sm line-through text-gray-500">$1000</span>
+                    </div>
+                    <div className="flex items-end">
+                        <span className="text-sm bg-primary p-1 rounded-2xl badge-primary h-min">-40%</span>
+                    </div>
+                </div>
+                    
+                
+            </div>
+        </div>
+    )
+}
+
+export default ProductItem;
--- src/components/products/productsRandom/ProductsRandom.tsx
+++ src/components/products/productsRandom/ProductsRandom.tsx
@@ -4,44 +4,20 @@ import ProductRandom from "./ProductsRandom.module.css"
 import { FaHeart } from "react-icons/fa6";
 import { FaCartArrowDown } from "react-icons/fa6";
 import { FaEye } from "react-icons/fa";
+import ProductItem from "./ProductItem";
 
 function ProductsRandom() {
     return (
         <>
-            <div className="w-full h-auto my-2 p-2">
-                <div className="w-72 bg-base-200  drop-shadow-md rounded-lg">
-                    <div className={`${ProductRandom.conteinerImg} relative overflow-hidden rounded-tl-lg rounded-tr-lg`}>
-                        <img
-                            className={`${ProductRandom.imgBx} hover:scale-110 transition-transform relative w-full h-auto object-cover rounded-tl-lg rounded-tr-lg select-none`}
-                            src="https://www.lemeilleuravis.com/wp-content/uploads/2020/08/vibox-pyro-sa4-133-pc-gamer-10.jpg"
-                            alt="Descripción de la imagen"
-                        />
-                        <ul className={`${ProductRandom.action} absolute`}>
-                            <li className="relative list-none w-10 h-10 bg-white flex justify-center items-center m-2 cursor-pointer text-sm rounded-lg group">
-                                <FaHeart className="text-primary" />
-                                <span className="absolute right-12 top-5 transform -translate-y-1/2 whitespace-nowrap px-10 py-2 bg-white text-primary rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ">Agregar a favoritos.</span>
-                            </li>
-                            <li className="relative list-none w-10 h-10 bg-white flex justify-center items-center m-2 cursor-pointer text-sm rounded-lg group">
-                                <FaCartArrowDown className="text-primary" />
-                                <span className="absolute right-12 top-5 transform -translate-y-1/2 whitespace-nowrap px-10 py-2 bg-white text-primary rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">Agregar al carrito.</span>
-                            </li>
-                           
-                        </ul>
-
-                    </div>
-
-
-                    <div className="px-5 py-3 space-y-2">
-                        <h3 className="text-lg">Super computadora 64GB</h3>
-                        <div className="space-x-2">
-                        </div>
-                        <p className="space-x-2">
-                            <span className="text-2xl font-semibold text-primary">$600</span>
-                            <span className="text-sm line-through text-gray-500">$1000</span>
-                            <span className="text-sm bg-primary p-1 rounded-2xl badge-primary mx-10">40% off</span>
-                        </p>
-                    </div>
-                </div>
+            <div className="w-full h-auto my-2 p-2 flex flex-wrap">
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
+                <ProductItem />
             </div>
         </>
     );
