5b8b0e8833e0265fd6fa842b724b3783b3e2d8e0
bruno10-0 <brunobrodon75@gmail.com>
2024-01-04 03:41:15 -0300

    feat: :lipstick: nuevo estilo al componente de producto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
         </Head>
 
       <body className={inter.className}>
-      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
         <Navbar />
         <main className="pt-28">
           {children}
