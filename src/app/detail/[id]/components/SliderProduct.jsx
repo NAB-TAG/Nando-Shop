47f7292f6af73d940eaeb6ca65e0f966b309fa70
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-01-22 11:18:22 -0300

    refactor(product-detail): :bug: Arregle el error que impide deployar el proyecto
    Cambie la extension del archivo de tsx a jsx

--- src/app/detail/[id]/components/SliderProduct.jsx
+++ src/app/detail/[id]/components/SliderProduct.jsx
@@ -13,26 +13,11 @@ import 'swiper/css/thumbs';
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 import Image from 'next/image';
 
-interface SliderProps {
-    data: {href: string}[]
-}
 
-const SliderProduct: React.FC<SliderProps> = ({data}) => {
-    const imageRef = useRef<HTMLImageElement  | null>(null);
- 
-    
-    const handleMouseEnter = () => {
-        console.log('Mouse entered!');
-        if (imageRef.current) {
-            console.log(imageRef.current);
 
-            imageRef.current.style.width = "300px";
-            
-        }    
-          
-    }
+const SliderProduct = ({data}) => {
 
-    const [thumbsSwiper, setThumbsSwiper] = useState<typeof SwiperComponent | null>(null);
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
 
     return (
@@ -47,8 +32,8 @@ const SliderProduct: React.FC<SliderProps> = ({data}) => {
                 { data.map(( image, index ) => (        
                     <SwiperSlide key={index}>
                         {/* <img src={ image.href } /> */}
-                        <div className='w-full h-[400px] bg-zinc-700' onMouseEnter={handleMouseEnter}>
-                            <Image src={ image.href } fill alt="image" ref={imageRef} className='object-contain bg-zinc-700 mx-auto'/>
+                        <div className='w-full h-[400px] bg-zinc-700' >
+                            <Image src={ image.href } fill alt="image" className='object-contain bg-zinc-700 mx-auto'/>
                         </div>
                     </SwiperSlide>
                 )) }
